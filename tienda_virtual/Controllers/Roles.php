63ac9da01c40dc12dcb6ddb694bad5fdc0ce8451
brahian <brahianriveros12@gmail.com>
2021-05-04 17:05:04 -0400

    e

--- tienda_virtual/Controllers/Roles.php
+++ tienda_virtual/Controllers/Roles.php
@@ -9,8 +9,8 @@ class Roles extends Controllers{
 	{
 		$data['page_id']= 3;
 		$data['page_tag'] = "Roles Usuario";
-		$data['page_title'] = "Roles Usuario";
-		$data['page_name'] = "Roles Usuario <small> Tienda Virtual </small>";
+		$data['page_name'] = "rol_usuario";
+		$data['page_title'] = "Roles Usuario <small> Tienda Virtual </small>";
 		$this->views->getView($this,"roles",$data);
 	}
 
