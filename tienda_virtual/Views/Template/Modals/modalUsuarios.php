218574a08153b51e7775b3e9c7417256efe5f1d4
brahian <brahianriveros12@gmail.com>
2021-05-06 09:17:53 -0400

    actualizacion 3

--- tienda_virtual/Views/Template/Modals/modalUsuarios.php
+++ tienda_virtual/Views/Template/Modals/modalUsuarios.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="modalFormUsuario" tabindex="-1" role="dialog"  aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-dialog modal-lg" >
     <div class="modal-content">
       <div class="modal-header headerRegister">
         <h5 class="modal-title" id="titleModal">Nuevo Usuario</h5>
